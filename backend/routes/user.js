80a422b2ab6614c8b1741fef5db8b42999344854
dedatekk <dedatekk@gmail.com>
2025-09-24 01:11:40 +1000

    fully CRUD bro

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -5,26 +5,122 @@ const User = require("./../models/User")
 
 // GET - get all users ---------------------------------------------------------------------------------
 router.get("/", async (req, res) => {
-    // get all users from the User model, using the find() method
-    User.find()
+  // get all users from the User model, using the find() method
+  User.find()
     //filtering values
-   //User.find({firstname: "David", accessLevel: 3})
-    .then((users) =>{
-        res.json(users)
-    })  
-    .catch(()=> {
-        console.log("problem getting users", err)
+    //User.find({firstname: "David", accessLevel: 3})
+    .then((users) => {
+      res.json(users)
     })
-
-   // GET - get a single user by id -----------------------------------------------------------------------
-   //endpoint = /user/:id
-   //Charlie Chaplin id =  68b988eeac33060f874b1fe6
-    router.get("/:id", (req, res) => {
-    console.log("[user/:id] param =", req.params.id)
-    //res.send(`get single user with id = ${req.params.id}`)
+    .catch((err) => {
+      console.log("error getting user", err)
+      res.status(500).json({
+        message: "problem getting user",
+        error: err,
+      })
+    })
 })
-   
-    
+
+// GET - get a single user by id -----------------------------------------------------------------------
+router.get("/:id", (req, res) => {
+  User.findById(req.params.id)
+    .then((user) => {
+      if (!user) {
+        return res.status(404).json({
+          message: "user doesn't exist",
+        })
+      } else {
+        res.json(user)
+      }
+    })
+    .catch((err) => {
+      res.status(500).json({ error: "error getting user" })
+    })
+})
+
+// POST - create new user ------------------------------------------------------
+router.post("/", (req, res) => {
+  // check if the req.body is empty, if so – send back error
+  if (!req.body) {
+    return res.status(400).json({
+      message: "user content is empty!",
+    })
+  }
+
+  // create a new user document using the User model
+  const newUser = new User({
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    accessLevel: req.body.accessLevel,
+    password: req.body.password,
+  })
+
+  // save newUser document to the database
+  newUser
+    .save()
+    .then((user) => {
+      res.status(201).json(user)
+    })
+    .catch((err) => {
+      console.log("error creating user", err)
+      res.status(500).json({
+        message: "problem creating user",
+        error: err,
+      })
+    })
+})
+
+// PUT - update user by id ==================================================
+// endpoint = /user/:id
+router.put("/:id", (req, res) => {
+  // check if the req.body is empty, if so – send back error
+  if (!req.body) {
+    return res.status(400).json({
+      message: "user content is empty!",
+    })
+  }
+
+  // update user using the User model
+  User.findByIdAndUpdate(req.params.id, req.body, { new: true })
+    .then((user) => {
+      res.json(user)
+    })
+    .catch((err) => {
+      console.log("error updating user", err)
+      // send back 500 status with error message
+      res.status(500).json({
+        message: "problem updating user",
+        error: err,
+      })
+    })
+})
+
+// DELETE - delete user by id
+// endpoint = /user/:id
+router.delete("/:id", (req, res) => {
+  // validate the request (make sure id isn't missing)
+  if (!req.params.id) {
+    return res.status(400).json({
+      message: "user id is missing!",
+    })
+  }
+
+  // delete the user using the User model
+  User.findOneAndDelete({ _id: req.params.id })
+    .then(() => {
+      res.json({
+        message: "User deleted",
+      })
+    })
+    .catch((err) => {
+      console.log("error deleting user", err)
+      // send back 500 status with error message
+      res.status(500).json({
+        message: "problem deleting user",
+        error: err,
+      })
+    })
 })
 
 module.exports = router
